df0f6523a7b973919a6ca1cf7f84c9004999f79f
Jacob McGladdery <jacobm117@gmail.com>
2016-07-04 21:18:07 -0400

    Fixed whitespace
    Using 4 spaces instead of tabs.

--- m.lua
+++ m.lua
@@ -45,11 +45,11 @@ m.VERSION = "0.1.0"
 -- Functions
 
 function m.isEven(value)
-	return value % 2 == 0
+    return value % 2 == 0
 end
 
 function m.isOdd(value)
-	return value % 2 == 1
+    return value % 2 == 1
 end
 
 function m.clamp(minimal, maximal, value)
@@ -71,11 +71,11 @@ function m.sum(array)
 end
 
 function m.product(array)
-	local result = 1
-	for _, value in ipairs(array) do
-		result = result * value
-	end
-	return result
+    local result = 1
+    for _, value in ipairs(array) do
+        result = result * value
+    end
+    return result
 end
 
 -- Statistics
@@ -87,67 +87,67 @@ function m.stat.mean(array)
 end
 
 function m.stat.median(array, isSorted)
-	if not isSorted then
-		sort(array)
-	end
-	local length = #array
-	local midpoint = length / 2
-	if m.isOdd(length) then
-		return array[ceil(midpoint)]
-	end
-	return (array[midpoint] + array[midpoint + 1]) / 2
+    if not isSorted then
+        sort(array)
+    end
+    local length = #array
+    local midpoint = length / 2
+    if m.isOdd(length) then
+        return array[ceil(midpoint)]
+    end
+    return (array[midpoint] + array[midpoint + 1]) / 2
 end
 
 function m.stat.mode(array)
-	local counts = {}
-	for _, v in ipairs(array) do
-		local value = counts[v]
-		if nil == value then
-			counts[v] = 1
-		else
-			counts[v] = value + 1
-		end
-	end
-	local mostFrequent = 0
-	for k, v in pairs(counts) do
-		if v > mostFrequent then
-			mostFrequent = v
-		end
-	end
-	local result = {}
-	for k, v in pairs(counts) do
-		if v == mostFrequent then
-			insert(result, k)
-		end
-	end
-	return result
+    local counts = {}
+    for _, v in ipairs(array) do
+        local value = counts[v]
+        if nil == value then
+            counts[v] = 1
+        else
+            counts[v] = value + 1
+        end
+    end
+    local mostFrequent = 0
+    for k, v in pairs(counts) do
+        if v > mostFrequent then
+            mostFrequent = v
+        end
+    end
+    local result = {}
+    for k, v in pairs(counts) do
+        if v == mostFrequent then
+            insert(result, k)
+        end
+    end
+    return result
 end
 
 function m.stat.deviation(value, mean)
-	return pow(value - mean, 2)
+    return pow(value - mean, 2)
 end
 
 function m.stat.variance(array)
-	local sum = 0
-	local mean = m.stat.mean(array)
-	for _, value in ipairs(array) do
-		sum = sum + m.stat.deviation(value, mean)
-	end
-	return (1 / #array) * sum
+    local sum = 0
+    local mean = m.stat.mean(array)
+    for _, value in ipairs(array) do
+        sum = sum + m.stat.deviation(value, mean)
+    end
+    return (1 / #array) * sum
 end
 
 function m.stat.standardDeviation(array)
-	return sqrt(m.stat.variance(array))
+    return sqrt(m.stat.variance(array))
 end
 
 function m.stat.minMax(array)
-	local maximal = -huge
-	local minimal = huge
-	for _, value in ipairs(array) do
-		maximal = max(maximal, value)
-		minimal = min(minimal, value)
-	end
-	return minimal, maximal
+    local maximal = -huge
+    local minimal = huge
+    for _, value in ipairs(array) do
+        maximal = max(maximal, value)
+        minimal = min(minimal, value)
+    end
+    return minimal, maximal
 end
 
 -- Moving Averages
@@ -166,14 +166,14 @@ function m.stat.average.simple(window)
 end
 
 function m.stat.average.cumulative()
-	local previous = 0
-	local n = 1
-	return function(value)
-		local result = (value + ((n - 1) * previous)) / n
-		previous = result
-		n = n + 1
-		return result
-	end
+    local previous = 0
+    local n = 1
+    return function(value)
+        local result = (value + ((n - 1) * previous)) / n
+        previous = result
+        n = n + 1
+        return result
+    end
 end
 
 -- Return the module
